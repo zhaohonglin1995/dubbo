f50d67c86c53c79dd364826c94b76dc33e65400f
赵红林(OF1116) <zhaohonglin@qianmi.com>
2016-02-03 15:08:40 +0800

    fix

--- dubbo-consumer/src/main/java/TestDubboProvider.java
+++ dubbo-consumer/src/main/java/TestDubboProvider.java
@@ -2,15 +2,16 @@ import com.github.zhaohonglin1995.dubbo.provider.api.ITestProvider;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * 描述:
+ * 描述:dubbo-Consumer调用方
  *
- * @author 赵红林(OF1116)
  */
 public class TestDubboProvider {
     public static void main(String[] args) {
+        //开始调用
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config.xml");
         ITestProvider iTestProvider=ctx.getBean(ITestProvider.class);
-        String str=iTestProvider.sayHi("Hi~");
+        //只负责调用这个接口方法,不需要关注具体实现
+        String str=iTestProvider.sayHi("dubbo");
         System.out.println(str);
     }
 }
--- dubbo-provider/dubbo-provider-impl/src/main/java/com/github/zhaohonglin1995/dubbo/provider/impl/ITestProviderImpl.java
+++ dubbo-provider/dubbo-provider-impl/src/main/java/com/github/zhaohonglin1995/dubbo/provider/impl/ITestProviderImpl.java
@@ -3,12 +3,11 @@ package com.github.zhaohonglin1995.dubbo.provider.impl;
 import com.github.zhaohonglin1995.dubbo.provider.api.ITestProvider;
 
 /**
- * 描述:
- *
- * @author 赵红林(OF1116)
+ * 描述:interface的具体实现类,consumer方并不关心
  */
 public class ITestProviderImpl implements ITestProvider {
+    //sayHi方法
     public String sayHi(String name) {
-        return "dubbo," + name;
+        return "hello," + name;
     }
 }
